de07c138d3325256c3f0e847ee4e7bb5f0022ee7
dancho12 <dancho1282@gmail.com>
2018-10-20 16:35:55 +0300

    Полностью доделал списки

--- app/src/main/java/com/example/danch/kanarand/MainActivity.java
+++ app/src/main/java/com/example/danch/kanarand/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    String[] cities = {"A", "KA", "SA", "TA", "NA", "HA","MA","YA","RA","WA","11"};
+    String[] cities = {"A", "KA", "SA", "TA", "NA", "HA","MA","YA","RA","WA","WO"};
     TextView selection;
     String item;
     private int selectedTest;
@@ -100,22 +100,43 @@ public class MainActivity extends AppCompatActivity {
                 if (r == 1) {
                     a++;
                     if (a < 6) {
-                        Imput(strLine);
+
+                            Imput(strLine);
                     }
-                    Log.d("Что там с счетчиком1", "a=" + a);
                 }
                 if (r == 6) {
                     a++;
                     if (a > 5 && a < 11) {
-                        Imput(strLine);
+
+                            Imput(strLine);
+
                     }
-                    Log.d("Что там с счетчиком2", "a=" + a);
+                }
+                if (r == 2) {
+                    a++;
+                    if (a < 42) {
+
+                        Imput(strLine);
+
+                    }
+                }
+                if (r == 3) {
+                    a++;
+                    if (a > 41 && a < 81) {
+
+                        Imput(strLine);
+
+                    }
                 }
                 b++;
-                    Log.d("Что там с счетчиком3", "b=" + b);
-                    if (b > 10) {
-                        Imput2(strLine);
+                    if (b > 10&&(r==6||r==1)) {
+
+                            Imput2(strLine);
                     }
+                if (b > 82&&(r==2||r==3)) {
+
+                    Imput2(strLine);
+                }
             }
             fstream.close();
         }catch (IOException e){
@@ -129,11 +150,45 @@ public class MainActivity extends AppCompatActivity {
         Log.d("onClickRadioGroupSelectTest", "selectedTest="+selectedTest);
     }
 
+    public void full_rand(View v)
+    {
+        Clean();
+        int r = 0;
+        Switch simpleSwitch = (Switch) findViewById(R.id.switch1);
+        Boolean switchState = simpleSwitch.isChecked();
+        switch (selectedTest) {
+            case R.id.radioButton1: {
+                        r = 2;
+                        InputStream fstream1 = getResources().openRawResource(R.raw.full);
+                        readFile(fstream1, r);
+                        break;
+            }
+            case R.id.radioButton2: {
+
+                    r = 3;
+                    InputStream fstream1 = getResources().openRawResource(R.raw.full);
+                    readFile(fstream1, r);
+                    break;
+            }
+        }
+
+        selection = (TextView) findViewById(R.id.textView2);
+        Rand();
+        if (switchState == (true)) {
+            selection.setText(OutPutLat());
+            ch=1;
+        } else {
+            selection.setText(OutPutRand());
+            ch=0;
+        }
+    }
+
     public void fu(View v) {
         Clean();
         int r = 0;
         Switch simpleSwitch = (Switch) findViewById(R.id.switch1);
         Boolean switchState = simpleSwitch.isChecked();
+
 
 
         switch (selectedTest) {
@@ -142,15 +197,60 @@ public class MainActivity extends AppCompatActivity {
 
                     case "A":
                         r = 1;
-                        InputStream fstream3 = getResources().openRawResource(R.raw.a);
-                        readFile(fstream3, r);
+                        InputStream fstream1 = getResources().openRawResource(R.raw.a);
+                        readFile(fstream1, r);
                         break;
                     case "KA":
                         r = 1;
-                        InputStream fstream6 = getResources().openRawResource(R.raw.ka);
-                        readFile(fstream6, r);
+                        InputStream fstream2 = getResources().openRawResource(R.raw.ka);
+                        readFile(fstream2, r);
                         break;
 
+                    case "SA":
+                        r = 1;
+                        InputStream fstream3 = getResources().openRawResource(R.raw.sa);
+                        readFile(fstream3, r);
+                        break;
+                    case "TA":
+                        r = 1;
+                        InputStream fstream5 = getResources().openRawResource(R.raw.ta);
+                        readFile(fstream5, r);
+                        break;
+                    case "NA":
+                        r = 1;
+                        InputStream fstream6 = getResources().openRawResource(R.raw.na);
+                        readFile(fstream6, r);
+                        break;
+                    case "HA":
+                        r = 1;
+                        InputStream fstream7 = getResources().openRawResource(R.raw.ha);
+                        readFile(fstream7, r);
+                        break;
+                    case "MA":
+                        r = 1;
+                        InputStream fstream8 = getResources().openRawResource(R.raw.ma);
+                        readFile(fstream8, r);
+                        break;
+                    case "YA":
+                        r = 1;
+                        InputStream fstream9 = getResources().openRawResource(R.raw.ya);
+                        readFile(fstream9, r);
+                        break;
+                    case "RA":
+                        r = 1;
+                        InputStream fstream10 = getResources().openRawResource(R.raw.ra);
+                        readFile(fstream10, r);
+                        break;
+                    case "WA":
+                        r = 1;
+                        InputStream fstream11 = getResources().openRawResource(R.raw.wa);
+                        readFile(fstream11, r);
+                        break;
+                    case "WO":
+                        r = 1;
+                        InputStream fstream12 = getResources().openRawResource(R.raw.wo);
+                        readFile(fstream12, r);
+                        break;
 
                 }
                 break;
@@ -161,13 +261,59 @@ public class MainActivity extends AppCompatActivity {
 
                     case "A":
                         r = 6;
-                        InputStream fstream3 = getResources().openRawResource(R.raw.a);
-                        readFile(fstream3, r);
+                        InputStream fstream1 = getResources().openRawResource(R.raw.a);
+                        readFile(fstream1, r);
                         break;
                     case "KA":
                         r = 6;
-                        InputStream fstream4 = getResources().openRawResource(R.raw.ka);
-                        readFile(fstream4, r);
+                        InputStream fstream2 = getResources().openRawResource(R.raw.ka);
+                        readFile(fstream2, r);
+                        break;
+
+                    case "SA":
+                        r = 6;
+                        InputStream fstream3 = getResources().openRawResource(R.raw.sa);
+                        readFile(fstream3, r);
+                        break;
+                    case "TA":
+                        r = 6;
+                        InputStream fstream5 = getResources().openRawResource(R.raw.ta);
+                        readFile(fstream5, r);
+                        break;
+                    case "NA":
+                        r = 6;
+                        InputStream fstream6 = getResources().openRawResource(R.raw.na);
+                        readFile(fstream6, r);
+                        break;
+                    case "HA":
+                        r = 6;
+                        InputStream fstream7 = getResources().openRawResource(R.raw.ha);
+                        readFile(fstream7, r);
+                        break;
+                    case "MA":
+                        r = 6;
+                        InputStream fstream8 = getResources().openRawResource(R.raw.ma);
+                        readFile(fstream8, r);
+                        break;
+                    case "YA":
+                        r = 6;
+                        InputStream fstream9 = getResources().openRawResource(R.raw.ya);
+                        readFile(fstream9, r);
+                        break;
+                    case "RA":
+                        r = 6;
+                        InputStream fstream10 = getResources().openRawResource(R.raw.ra);
+                        readFile(fstream10, r);
+                        break;
+                    case "WA":
+                        r = 6;
+                        InputStream fstream11 = getResources().openRawResource(R.raw.wa);
+                        readFile(fstream11, r);
+                        break;
+                    case "WO":
+                        r = 6;
+                        InputStream fstream12 = getResources().openRawResource(R.raw.wo);
+                        readFile(fstream12, r);
                         break;
                 }
                 break;
--- app/src/main/jni/native-lib.c
+++ app/src/main/jni/native-lib.c
@@ -8,3 +8,12 @@ Java_com_example_danch_kanarand_MainActivity_stringFromJNI(JNIEnv *env, jobject 
 
     return (*env)->NewStringUTF(env, returnValue);
 }
+
+JNIEXPORT jstring JNICALL
+Java_com_example_danch_kanarand_MainActivity_stringFromJNI(JNIEnv *env, jobject instance) {
+
+    // TODO
+
+
+    return (*env)->NewStringUTF(env, returnValue);
+}
